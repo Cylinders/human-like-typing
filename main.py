a1ffad01dbf0ccd780311925124aae34ef841e48
Ruthvik Venkatesan <65773092+Cylinders@users.noreply.github.com>
2024-02-20 20:59:09 -0500

    Simple randomization and missed keys.

--- main.py
+++ main.py
@@ -1,19 +1,49 @@
 import pyautogui as gui 
 import time
+import random
+import string
 
 
 content = input("Copy and paste the prompt into here. Then, you will have five seconds to move your mouse over to the document writing application in order to simulate the typing.").split()
 
 time.sleep(5)
-try: 
-        
-    for word in content: 
-        gui.typewrite(word)
+
+def tenpercentchance(): 
+    return random.random() > .9
+
+def sevenpercentchance(): 
+     return random.random() > .93
+
+def randomCharacter(): 
+     return random.choice(string.ascii_letters)
+
+for word in content: 
+        for letter in word: 
+            gui.typewrite(letter)
+            time.sleep(.1 * random.random())
+            
+            if tenpercentchance(): 
+                time.sleep(.5 * random.random())
+
+            if sevenpercentchance(): 
+                if (random.randint(1, 2) == 1): 
+                    gui.typewrite(randomCharacter())
+                    time.sleep(.1 * random.random())
+                    gui.press('backspace')
+                else: 
+                    gui.typewrite(randomCharacter())
+                    time.sleep(.1 * random.random())
+                    gui.typewrite(randomCharacter())
+                    time.sleep(.1 * random.random())
+                    gui.press('backspace')
+                    time.sleep(.1 * random.random())
+                    gui.press('backspace')
         gui.typewrite(" ")
-        time.sleep(.1)
-except: 
-    print("ERROR")
-    exit
+        time.sleep(.3 * random.random())
+        
+        if tenpercentchance(): 
+            time.sleep(.7 * random.random())
+
 
 
 print("Program completed succesfully. EXIT. EXIT.")
